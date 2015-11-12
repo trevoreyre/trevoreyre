0d732d37992607c135064a9e2122a769924297f0
Trevor Eyre <trevoreyre@gmail.com>
2015-11-12 11:13:26 -0800

    Modified functionality of drawer panel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,18 @@
 // main.js
 
+// Scripts for Polymer elements
+document.addEventListener('WebComponentsReady', function () {
+    (function (global) {
+        'use strict';
+        var drawerPanel = document.getElementById('te-paper-drawer-panel');
+
+        global.teToggleDrawer = function () {
+            if (drawerPanel.narrow && drawerPanel.offsetWidth < parseInt(drawerPanel.responsiveWidth)) {
+                drawerPanel.togglePanel();
+            } else {
+                drawerPanel.forceNarrow = !drawerPanel.forceNarrow;
+            }
+        }
+    }(this));
+
+});
