3c024af5fdc99cab31005e80be5953fafcf9c01d
Trevor Eyre <trevoreyre@gmail.com>
2015-11-20 09:39:01 -0800

    Updated main page layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,19 @@
 document.addEventListener('WebComponentsReady', function () {
     (function (global) {
         'use strict';
-        var drawerPanel = document.getElementById('te-paper-drawer-panel');
+        var drawerPanel = document.getElementById('te-drawer-panel');
+        var menu = document.getElementById('te-menu');
 
+        // Handle menu click event
+        menu.addEventListener('iron-select', function () {
+            console.log(menu.selected);
+        });
+
+        // Change drawer panel breakpoint, and width
+        drawerPanel.drawerWidth = '208px';
+        drawerPanel.responsiveWidth = '840px';
+
+        // Drawer panel toggle
         global.teToggleDrawer = function () {
             if (drawerPanel.narrow && drawerPanel.offsetWidth < parseInt(drawerPanel.responsiveWidth)) {
                 drawerPanel.togglePanel();
