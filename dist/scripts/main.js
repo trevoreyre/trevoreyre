79d6e32d795bace177ca6dfc2e768b8597044960
Trevor Eyre <trevoreyre@gmail.com>
2015-12-05 09:01:16 -0800

    Moved compiled files to dist folder. Restructured style files.

---
+++ dist/scripts/main.js
@@ -0,0 +1,75 @@
+// main.js
+
+// Scripts for Polymer elements
+document.addEventListener('WebComponentsReady', function () {
+    (function (global) {
+        'use strict';
+        var drawerPanel = document.getElementById('te-drawer-panel');
+        var menu = document.getElementById('te-menu');
+
+        // Highlight selected page in menu
+        switch (window.location.pathname) {
+            case '/trevoreyre/portfolio/':
+                menu.selected = 1;
+                break;
+            case '/trevoreyre/contact/':
+                menu.selected = 2;
+                break;
+            default:
+                menu.selected = 0;
+        }
+
+        // Handle menu click event
+        menu.addEventListener('iron-select', function () {
+            switch (menu.selected) {
+                case 0:
+                    window.location.href = '/trevoreyre/';
+                    break;
+                case 1:
+                    window.location.href = '/trevoreyre/portfolio/';
+                    break;
+                case 2:
+                    window.location.href = '/trevoreyre/contact/';
+                    break;
+            }
+        });
+
+        // Change drawer panel breakpoint, and width
+        drawerPanel.drawerWidth = '208px';
+        drawerPanel.responsiveWidth = '840px';
+
+        // Drawer panel toggle
+        global.teToggleDrawer = function () {
+            if (drawerPanel.narrow && drawerPanel.offsetWidth < parseInt(drawerPanel.responsiveWidth)) {
+                drawerPanel.togglePanel();
+            } else {
+                drawerPanel.forceNarrow = !drawerPanel.forceNarrow;
+            }
+        }
+    }(this));
+
+});
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,14 @@ var gutil = require('gulp-util');
 var ftp = require('vinyl-ftp');
 var concat = require('gulp-concat');
 var cssmin = require('gulp-minify-css');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var wrap = require('gulp-wrap');
 
-// Require config file
+// Require config file for FTP server info
 var config = require('./config.json');
 
 // Default task
@@ -31,114 +33,115 @@ var conn = ftp.create({
 // Directory to deploy files
 var deployDestination = config.deployDestination;
 
-// Files and folders to ignore in deploy tasks
-var deployIgnoreFiles = [
-    '!./bower_components/',
-    '!./bower_components/**',
-    '!./node_modules/',
-    '!./node_modules/**',
-    '!./src/',
-    '!./src/**',
-    '!./bower.json',
-    '!./package.json',
-    '!./gulpfile.js',
-    '!./config.json',
-    '!./README.md'
-];
-
-// Deploy all files
-gulp.task('deployAll', ['layout', 'scripts', 'styles', 'polymerStyles'], function () {
+// Deploy all files. Simply kicks off all other deploy tasks. Also uploads any new image files.
+gulp.task('deployAll', ['deployLayout', 'deployScripts', 'deployStyles'], function () {
     var deployFiles = [
-        './**'
+        './dist/img/*'
     ];
-    var deployFiles = deployFiles.concat(deployIgnoreFiles);
 
     return gulp.src(deployFiles, {base: '.', buffer: false})
+        .pipe(rename(function (path) {
+            var parts = path.dirname.split('\\');
+            parts.splice(0, 1);
+            path.dirname = parts.join('\\');
+        }))
         .pipe(conn.newer(deployDestination))
         .pipe(conn.dest(deployDestination));
 });
 
-// Deploy changed html files
+// Deploy changed html files after running 'layout' task
 gulp.task('deployLayout', ['layout'], function () {
     var deployFiles = [
-        './**/*.html',
-        '!./styles/**'
+        './dist/**/*.html',
+        '!./dist/styles/**/*' // Ignore html files defining Polymer styles
     ];
-    var deployFiles = deployFiles.concat(deployIgnoreFiles);
 
     return gulp.src(deployFiles, {base: '.', buffer: false})
+        .pipe(rename(function (path) {
+            var parts = path.dirname.split('\\');
+            parts.splice(0, 1);
+            path.dirname = parts.join('\\');
+        }))
         .pipe(conn.newer(deployDestination))
         .pipe(conn.dest(deployDestination));
 });
 
-// Deploy changed javascript files
+// Deploy changed javascript files after running 'scripts' task
 gulp.task('deployScripts', ['scripts'], function () {
     var deployFiles = [
-        './scripts/**'
+        './dist/scripts/**/*.min.js'
     ];
-    var deployFiles = deployFiles.concat(deployIgnoreFiles);
 
     return gulp.src(deployFiles, {base: '.', buffer: false})
+        .pipe(rename(function (path) {
+            var parts = path.dirname.split('\\');
+            parts.splice(0, 1);
+            path.dirname = parts.join('\\');
+        }))
         .pipe(conn.newer(deployDestination))
         .pipe(conn.dest(deployDestination));
 });
 
-// Deploy changed style files
-gulp.task('deployStyles', ['styles', 'polymerStyles'], function () {
+// Deploy changed style files after running 'styles' task
+gulp.task('deployStyles', ['styles'], function () {
     var deployFiles = [
-        './styles/**'
+        './dist/styles/**/*.min.css',
+        './dist/styles/**/*.html' // Polymer styles
     ];
-    var deployFiles = deployFiles.concat(deployIgnoreFiles);
 
     return gulp.src(deployFiles, {base: '.', buffer: false})
+        .pipe(rename(function (path) {
+            var parts = path.dirname.split('\\');
+            parts.splice(0, 1);
+            path.dirname = parts.join('\\');
+        }))
         .pipe(conn.newer(deployDestination))
         .pipe(conn.dest(deployDestination));
 });
 
 // HTML layout task
 gulp.task('layout', function () {
-    return gulp.src(['./src/**/*.html', '!./src/layout.html', '!./src/styles/**'])
+    return gulp.src(['./src/**/*.html', '!./src/layout.html', '!./src/styles/**/*'])
         .pipe(wrap({src: './src/layout.html'}))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./dist/'));
 });
 
 // Scripts task
 gulp.task('scripts', function () {
-    return gulp.src('./src/scripts/*.js')
+    return gulp.src('./src/scripts/**/*.js')
         .pipe(concat('main.js'))
+        .pipe(gulp.dest('./dist/scripts/'))
         .pipe(uglify())
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('./scripts/'));
+        .pipe(gulp.dest('./dist/scripts/'));
 });
 
 // Styles task
 gulp.task('styles', function () {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['> 2%']
+        }))
+        .pipe(gulp.dest('./dist/styles/'))
         .pipe(cssmin())
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('./styles/'));
+        .pipe(gulp.dest('./dist/styles/'));
 });
 
-// Polymer styles task. Compiles SASS and wraps in Polymer template
+// Polymer styles task. Just pushes Polymer styles to dist folder
 gulp.task('polymerStyles', function () {
-    return gulp.src('./src/styles/*.html')
-        /*.pipe(sass())
-        .pipe(cssmin())
-        .pipe(wrap({src: './src/styles/polymer/te-polymer-styles-wrap.html'}))
-        .pipe(rename(function (path) {
-            path.extname = '.html'
-        }))*/
-        .pipe(gulp.dest('./styles/'));
+    return gulp.src('./src/styles/**/*.html')
+        .pipe(gulp.dest('./dist/styles/'));
 });
 
 // Watch task
 gulp.task('watch', function () {
-    gulp.watch(['./src/**/*.html', '!./src/styles/**'], ['deployLayout']);
-    gulp.watch('./src/scripts/**', ['deployScripts']);
-    gulp.watch('./src/styles/**', ['deployStyles']);
+    gulp.watch(['./src/**/*.html', '!./src/styles/**/*'], ['deployLayout']);
+    gulp.watch('./src/scripts/**/*', ['deployScripts']);
+    gulp.watch('./src/styles/**/*', ['deployStyles']);
 });
