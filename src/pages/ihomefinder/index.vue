71cb35081153b066b0075f11acfa73ccfc5d5085
Trevor Eyre <trevoreyre@gmail.com>
2018-07-22 09:53:59 -0600

    iframe test for another website

---
+++ src/pages/ihomefinder/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class='container'>
+    <iframe class='ihomefinder-test'
+      src='https://idxhome.com/report/listing-report/By-Listing-Number/114717/660394'
+    >
+      <p>Your browser does not support iframes.</p>
+    </iframe>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ihomefinder'
+  }
+</script>
+
+<style lang='scss' scoped>
+  .container {
+    padding: 48px;
+  }
+
+  .ihomefinder-test {
+    width: 100%;
+    height: 800px;
+    border: none;
+  }
+</style>
