d3744be89cefd2deffb28af5b233ce3398122832
Trevor Eyre <trevoreyre@gmail.com>
2018-09-18 20:15:52 -0600

    Added autocomplete pen

--- src/common/pens.js
+++ src/common/pens.js
@@ -1,11 +1,9 @@
 export default [
+  'bvwOeL', // Autocomplete
   'eegqXP', // CSS masonry layout
   'OONEKM', // Site header halves
   'bRrrEx', // Frosted glass card overlay
   'pPpQbG', // Site header quadrants concept
   'dvNwqG', // Simple device mockups in CSS
-  'xgWKxB', // Featured posts slider
-  'zdKmOw', // Full page featured posts slider
-  'XpKNRm', // Sticky, self-hiding navigation
   'vXQRYo' // Floating mobile navigation
 ]
